ad10608b85aec299cc4af4be58840414d2d7b043
swathibulbul <51088242+swathibulbul@users.noreply.github.com>
2019-07-16 20:33:06 +0530

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,42 +1,31 @@
-'''class_list = dict()
-BookId = input('Enter column name & IdNumber separated by ":" \n')
-temp = BookId.split(':')
-class_list[temp[0]] = int(temp[1])
-BookName = input('Enter column name & BookName separated by ":" \n')
-temp1 = BookName.split(':')
-class_list[temp[1]] = str(temp1[2])
-AuthorName = input('Enter column name & AuthorName separated by ":" \n')
-temp2 = AuthorName.split(':')
-class_list[temp[2]] = str(temp2[3])
-price = input('Enter column name & BookName separated by ":" ')
-temp3 = price.split(':')
-class_list[temp[3]] = float(temp3[4])
- 
-# Displaying the dictionary
-for key, value in class_list.items():
-	print('BookId: {}, Id: {}'.format(key, value))
-    #print('BookName: {}, bname: {}'.format(key, value))
-    #print('AuthorName: {}, aname: {}'.format(key, value))
-    #print('Price: {}, bprice: {}'.format(key, value))'''
 
-colname1 = input("enter the key 1\n")
 Bookid = int(input("enter the bookid\n"))
-colname2=input("enter the columnname 2\n")
+
 bookname=input("enter the bookname\n")
-colname3=input("enter columnname 3\n")
+
 authorname=input("enter tke authorname\n")
-colname4=input("enter columnname 4\n")
+
 price=float(input("enter the price\n"))
 
 
 class_list = {}
 
-class_list[colname1] = Bookid
-class_list[colname2] = bookname
-class_list[colname3] = authorname
-class_list[colname4] = price
+class_list['BookId'] = Bookid
+class_list['BookName'] = bookname
+class_list['AuthorName'] = authorname
+class_list['Price'] = price
 
 
 print(class_list)
-#for book in class_list:
-    #print(book.items())
+
+------------------------o/p---------------------------
+
+enter the bookid
+1001
+enter the bookname
+Python
+enter tke authorname
+swathi
+enter the price
+1000
+{'BookId': 1001, 'BookName': 'Python', 'AuthorName': 'swathi', 'Price': 1000.0}
