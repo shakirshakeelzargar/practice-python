38cc035a322c058b0f5381bdef52d7a3445f5059
swathibulbul <51088242+swathibulbul@users.noreply.github.com>
2019-07-17 11:08:09 +0530

    Update dictionaries.py

--- swathi/dictionaries.py
+++ swathi/dictionaries.py
@@ -19,3 +19,15 @@ class_list['Price'] = price
 
 
 print(class_list)
+
+----------output--------------
+
+enter the bookid
+1100
+enter the bookname
+Python
+enter tke authorname
+Swathi
+enter the price
+2000
+{'BookId': 1100, 'BookName': 'Python', 'AuthorName': 'Swathi', 'Price': 2000.0}
